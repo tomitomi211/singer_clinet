246253a9715f2f12f7272f9e63538c0ff069f9ad
tomi <tomi@tominoMacBook-Air.local>
2024-02-03 23:39:46 +0900

    画面遷移の実装

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,39 @@
 import React from 'react';
-import logo from './logo.svg';
+// import logo from './logo.svg';
 import './App.css';
+
+import { BrowserRouter, Link, Route, Routes } from "react-router-dom";
+import "./App.css";
+
+import About from "./components/About";
+import Contact from "./components/Contact";
+import Home from "./components/Home";
+import Works from "./components/Works";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BrowserRouter>
+      <div className="App">
+        <Link to="/">Home</Link>
+        <br />
+        <Link to="/about">About</Link>
+        <br />
+        <Link to="/works">Works</Link>
+        <br />
+        <Link to="/contact">Contact</Link>
+        <br />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/about" element={<About />} />
+          <Route path="/works/" element={<Works />} />
+          <Route path="/contact" element={<Contact />} />
+        </Routes>
+        {/* <Footer /> */}
+      </div>
+    </BrowserRouter>
   );
 }
+
+
 
 export default App;
